0ccce3160da9a62a67204d22315a2dfe9f4815da
jenniekibiri <josphatje@gmail.com>
2020-06-11 06:47:10 -0400

    creact user login

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom'
 import Home from './core/Home'
 import Signup from './user/Signup'
+import Signin from './user/Signin'
+
  const MainRouter =()=>(
      <Switch>
       <div>
           <Route exact path="/" component={Home}/>
           <Route exact path="/signup" component={Signup}/>
+          <Route exact path="/signin" component={Signin}/>
 
       </div>
      </Switch>
