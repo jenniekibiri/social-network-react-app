b9d0478ef77264b08ebedb0236b2123d23668418
jenniekibiri <josphatje@gmail.com>
2020-06-11 06:59:25 -0400

    add menu component with navbar

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,15 +3,20 @@ import {Switch,Route} from 'react-router-dom'
 import Home from './core/Home'
 import Signup from './user/Signup'
 import Signin from './user/Signin'
-
+import Menu from './core/Menu'
  const MainRouter =()=>(
-     <Switch>
-      <div>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/signup" component={Signup}/>
-          <Route exact path="/signin" component={Signin}/>
 
-      </div>
-     </Switch>
+     <div>
+         <Menu/>
+         <Switch>
+   
+ 
+       <Route exact path="/" component={Home}/>
+       <Route exact path="/signup" component={Signup}/>
+       <Route exact path="/signin" component={Signin}/>
+
+
+  </Switch>
+     </div>
  ) 
  export default MainRouter;
---
+++ src/core/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+const Menu = () =>(
+
+<div>
+<Link to="/">Home</Link>
+<Link to="/signin">Signin</Link>
+<Link to="/signup">Signup</Link>
+</div>
+
+)
+export default Menu;
