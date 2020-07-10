e1b37e5800bfba7d5b30dfe9c52718ab82b1e0ac
jenniekibiri <josphatje@gmail.com>
2020-07-10 10:41:31 -0400

    conditional rendering on nav links

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-const Menu = () =>(
+import React, { Fragment } from "react";
+import { Link, useHistory, withRouter } from "react-router-dom";
+//next is the onclick link
+export const signout = (next) => {
+  if (window !== "undefined") localStorage.removeItem("jwt");
+  next();
+  return fetch("https://letsnetwork.herokuapp.com/signout", {
+    method: "POST",
+  })
+    .then((response) => {
+      console.log("signout", response);
+      return response.json();
+    })
+    .catch((err) => console.log(err));
+};
 
-<div>
+export const isAuthenticated = () => {
+  if (localStorage.getItem("jwt")) {
+    return JSON.parse(localStorage.getItem("jwt"));
+  } else {
+    return false;
+  }
+};
+function Menu() {
+  const history = useHistory();
+  return (
+    <div>
+      <ul className="nav nav-tabs bg-primary">
+        <li className="nav-item">
+          <Link className="nav-link " to="/">
+            Home
+          </Link>
+        </li>
+        {!isAuthenticated() && (
+          <Fragment>
+            <li className="nav-item">
+              <Link className="nav-link " to="/signin">
+                Signin
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link " to="/signup">
+                Signup
+              </Link>
+            </li>
+          </Fragment>
+        )}
 
-<ul className="nav nav-tabs bg-primary">
-  <li className="nav-item">
-    <Link className="nav-link active"  to="/">Home</Link>
-  </li>
-  
-  <li className="nav-item">
-    <Link className="nav-link "  to="/signin">Signin</Link>
-  </li>
-  <li className="nav-item">
-    <Link className="nav-link "  to="/signup">Signup</Link>
-  </li>
-</ul>
-</div>
+        {isAuthenticated() && (
+          <div>
+            <li className="nav-item">
+              <a
+                className="nav-link "
+                style={{ color: "white", cursor: "pointer" }}
+                onClick={() => signout(() => history.push("/"))}
+              >
+                Sign out
+              </a>
+            </li>
+          </div>
+        )}
+      </ul>
+    </div>
+  );
+}
 
-
-
-
-)
-export default Menu;
+export default withRouter(Menu);
