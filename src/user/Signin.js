e6b6ad8b81d0e255bfa414411e685b388d3e19fa
jenniekibiri <josphatje@gmail.com>
2020-07-13 03:51:02 -0400

    remove dotenv

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-const dotenv = require('dotenv')
-dotenv.config()
 class Signin extends Component {
   constructor() {
     super();
