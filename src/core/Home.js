92fbf26f2c4f15fc858affbed3896dfd67ea1891
jenniekibiri <josphatje@gmail.com>
2020-08-19 08:49:22 -0400

    show posts on home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,9 +2,10 @@ import React from 'react';
 import Posts from '../posts/posts'
 const Home = () =>(
     <div className="jumbotron">
-        <h3 className="display-4">Recent posts</h3>
+        <h3 className="display-4 ml-4">Recent posts</h3>
 <Posts/>
     </div>
 
 )
 export default Home;
+
--- src/posts/posts.js
+++ src/posts/posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { allPosts } from "./apiPosts";
 import { Link } from "react-router-dom";
+import DefaultPost from "../images/flat.jpeg"
 export class posts extends Component {
   constructor() {
     super();
@@ -24,30 +25,60 @@ export class posts extends Component {
         console.log(err);
       });
   }
-  showPosts = (posts) => (
-    <div className="row ">
-      {posts.map((post, i) => (
-        <div
-          className="card col-md-3 m-4 text-center"
-          key={i}
-          style={{ width: "18rem" }}
-        >
-          <div className="card-body">
-            <h5 className="card-title">{post.title}</h5>
+    showPosts = (posts) => {
+        return (
+            <div className="row">
+                {posts.map((post, i) => {
+                    const posterId = post.postedBy
+                        
+        
+                        ? `/user/${post.postedBy._id}`
+                        : "";
+                    const posterName = post.postedBy
+                        ? post.postedBy.name
+                        : " Unknown";
+                    return (
+      
+                     
+                        <div
+                            className="card col-md-4 m-2"
+                            key={i}
+                        >
+                             <img
+                                    src={`${
+                                        process.env.REACT_APP_API_URL
+                                    }/post/photo/${post._id}`}
+                                    alt={post.title}
+                                    onError={i =>
+                                        (i.target.src = `${DefaultPost}`)
+                                    }
+                                    className="img-thunbnail mb-3"
+                                    style={{ height: "200px", width: "100%" }}
+                                />
+                            <div className="card-body">
+                                <h5 className="card-title">{post.title}</h5>
            
-            <p className="card-text">{post.body}</p>
-            <Link
-              to={`/post/${post._id}`}
-              href="#"
-              className="btn btn-raised btn-info btn-sm"
-            >
-              Read More
+                                <p className="card-text">{post.body.substring(0,100)}</p>
+                                <p className="font-italic mark">
+                                    Posted by{" "}
+                                    <Link to={`${posterId}`}>
+                                        {posterName}{" "}
+                                    </Link>
+                                    on {new Date(post.created).toDateString()}
+                                </p>
+                                <Link
+                                    to={`/post/${post._id}`}
+                                    href="#"
+                                    className="btn btn-raised btn-info btn-sm"
+                                >
+                                    Read More
             </Link>
-              </div>
+                            </div>
+                        </div>
+                    )
+                } )}
         </div>
-      ))}
-    </div>
-  );
+       ) };
   render() {
     const { posts } = this.state;
     return (
