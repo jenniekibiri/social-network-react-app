df56e2dd6bb15a139c7ea2a5f0ee76f4828e0746
jenniekibiri <josphatje@gmail.com>
2020-08-19 09:20:05 -0400

    edit nav bar

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from "react";
 import { Link, useHistory, withRouter } from "react-router-dom";
 import { signout, isAuthenticated } from "../auth/auth";
-
+import '../css/style.css'
 function Menu() {
   const history = useHistory();
   return (
     <div>
-      <ul className="nav nav-tabs bg-info">
+      <ul className="nav nav-tabs shadow  rounded">
         <li className="nav-item">
-          <Link className="nav-link " to="/">
+          <Link className="nav-link  text-white" to="/">
             Home
           </Link>
         </li>
@@ -29,14 +29,11 @@ function Menu() {
 
         {isAuthenticated() && (
           <Fragment>
+         
             <li className="nav-item">
-              <a
-                className="nav-link "
-                style={{ color: "white", cursor: "pointer" }}
-                onClick={() => signout(() => history.push("/"))}
-              >
-                Sign out
-              </a>
+              <Link to="/createPosts" className="nav-link" style={{ color: "#fff" }}>
+                Create Posts
+              </Link>
             </li>
             <li className="nav-item">
               <Link to="/Users" className="nav-link" style={{ color: "#fff" }}>
@@ -51,6 +48,15 @@ function Menu() {
               >
                 {`${isAuthenticated().user.name}'s Profile`}
               </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link "
+                style={{ color: "white", cursor: "pointer" }}
+                onClick={() => signout(() => history.push("/"))}
+              >
+                Sign out
+              </a>
             </li>
           </Fragment>
         )}
