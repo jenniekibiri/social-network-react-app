027e9dd73340ea4129b2b5093883a9fb02076002
jenniekibiri <josphatje@gmail.com>
2020-08-20 02:19:51 -0400

    fix auth error

--- src/user/SocialLogin.js
+++ src/user/SocialLogin.js
@@ -1,55 +1,53 @@
-
 import React, { Component } from "react";
 import GoogleLogin from "react-google-login";
-import { socialLogin,authenticate}from '../auth/auth'
+import { socialLogin, authenticate } from "../auth/auth";
+import { Redirect } from "react-router-dom";
 class SocialLogin extends Component {
-    constructor() {
-        super();
-        this.state = {
-            redirectToReferrer: false
-        };
-    }
-
-    responseGoogle = response => {
-        console.log(response);
-        const { googleId, name, email, imageUrl } = response.profileObj;
-        const user = {
-            password: googleId,
-            name: name,
-            email: email,
-            imageUrl: imageUrl
-        };
-        socialLogin(user).then(data => {
-            console.log("signin data: ", data);
-            if (data.error) {
-                console.log("Error Login. Please try again..");
-            } else {
-                console.log("signin success - setting jwt: ", data);
-                authenticate(data, () => {
-                    this.setState({ redirectToReferrer: true });
-                });
-            }
-        });
-
-
+  constructor() {
+    super();
+    this.state = {
+      redirectToReferrer: false,
     };
+  }
 
-    render() {
-        const { redirectToReferrer } = this.state;
-        if (redirectToReferrer) {
-            return <Redirect to="/" />;
-        }
-        return (
-            <div className="container ">
-                <GoogleLogin
-                    clientId="51203616981-2s6o9pvrjsfihbsttj1b9qkhq51b9tah.apps.googleusercontent.com"
-                    buttonText="Login with Google"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle}
-                />
-            </div>
-        );
+  responseGoogle = (response) => {
+    console.log(response);
+    const { googleId, name, email, imageUrl } = response.profileObj;
+    const user = {
+      password: googleId,
+      name: name,
+      email: email,
+      imageUrl: imageUrl,
+    };
+    socialLogin(user).then((data) => {
+      console.log("signin data: ", data);
+      if (data.error) {
+        console.log("Error Login. Please try again..");
+      } else {
+        console.log("signin success - setting jwt: ", data);
+        authenticate(data, () => {
+          this.setState({ redirectToReferrer: true });
+        });
+      }
+    });
+  };
+
+  render() {
+    const { redirectToReferrer } = this.state;
+    if (redirectToReferrer) {
+      return <Redirect to="/" />;
     }
+    return (
+      <div className="container ">
+        <GoogleLogin
+          clientId="51203616981-2s6o9pvrjsfihbsttj1b9qkhq51b9tah.apps.googleusercontent.com"
+          buttonText="Login with Google"
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
+      </div>
+    );
+  }
 }
 
 export default SocialLogin;
